d26804b5724feeba4a8ef83754becbf16866ae0b
Rokas Ciapas <rokas.ciapas@nortal.com>
2022-08-04 14:50:19 +0300

    add sports days to sports plan

---
+++ lib/models/sports_day.dart
@@ -0,0 +1,9 @@
+import 'exercise.dart';
+
+class SportsDay {
+  final List<Exercise> exercises;
+
+  SportsDay({
+    required this.exercises
+  });
+}
--- lib/screens/edit_sports_plan.dart
+++ lib/screens/edit_sports_plan.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sapsak/models/client.dart';
 import 'package:sapsak/models/exercise.dart';
-import 'package:supercharged/supercharged.dart';
+import 'package:sapsak/models/sports_day.dart';
 
 class EditSportsPlan extends StatefulWidget {
   const EditSportsPlan({
@@ -18,28 +18,23 @@ class EditSportsPlan extends StatefulWidget {
 }
 
 class _EditSportsPlanState extends State<EditSportsPlan> {
-  final exercises = List.filled(1, Exercise(muscleGroup: 'Shoulders', name: '', repCount: 0, setCount: 0), growable: true);
+  final sportsDays = List.filled(1, SportsDay(exercises: List.filled(1, Exercise(muscleGroup: 'Shoulders', name: '', repCount: 0, setCount: 0), growable: true)), growable: true);
 
-  String dropdownValue = 'Shoulders';
   final ScrollController listViewScrollController = ScrollController();
-  final double _height = 180.0;
 
-  void animateToIndex(int index) {
+  void scrollDown() {
     listViewScrollController.animateTo(
-      index * _height,
-      duration: const Duration(seconds: 2),
-      curve: Curves.fastOutSlowIn,
+      9999999,
+      duration: const Duration(seconds: 1),
+      curve: Curves.linear,
     );
   }
 
   @override
   Widget build(BuildContext context) {
+
     final w = MediaQuery.of(context).size.width;
     final h = MediaQuery.of(context).size.height;
-
-    final exerciseController = TextEditingController();
-    final setCountController = TextEditingController();
-    final repCountController = TextEditingController();
 
     return Scaffold(
         appBar: AppBar(
@@ -53,123 +48,171 @@ class _EditSportsPlanState extends State<EditSportsPlan> {
             margin: const EdgeInsets.all(17),
             child: Column(
               children: [
-                Expanded(
+                Expanded(flex: 1,
                     child: ListView(
-                      controller: listViewScrollController,
-                      children: [
-
-                        for( int i = 0 ; i < exercises.length ; i++)
-                          Column(
-                            children: [
-                              CircleAvatar(child: Text((i + 1).toString())),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Row(
-                                children: [
-                                  Expanded(flex: 0, child: DropdownButton<String>(
-                                    value: exercises[i].muscleGroup,
-                                    hint: const Text('Muscle group'),
-                                    items: <String>['Shoulders', 'Biceps', 'Triceps', 'Chest', 'Abs', 'Back', 'Legs'].map((String value) {
-                                      return DropdownMenuItem<String>(
-                                        value: value,
-                                        child: Text(value),
-                                      );
-                                    }).toList(),
-                                    onChanged: (String? newValue) {
-                                      setState(() {
-                                        exercises[i] = Exercise(muscleGroup: newValue ?? '', name: exercises[i].name, repCount: exercises[i].repCount, setCount: exercises[i].setCount);
-
-                                      });
-                                    },
+                        shrinkWrap: true,
+                        controller: listViewScrollController,
+                        children: sportsDays.map((SportsDay sportsDay) =>
+                            Column(
+                              children: [
+                                SizedBox(height: 60,
+                                  child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text('Day ${sportsDays.indexOf(sportsDay) + 1}',
+                                          style: TextStyle(fontSize: 18, color: Theme.of(context).primaryColor)
+                                      )
                                   ),),
-                                  const SizedBox(
-                                    width: 15,
-                                  ),
-                                  Expanded(flex: 6, child: TextField(
-                                    onChanged: (text) {
-                                      exercises[i] = Exercise(muscleGroup: exercises[i].muscleGroup, name: text, repCount: exercises[i].repCount, setCount: exercises[i].setCount);
-                                    },
-                                    decoration: const InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      hintText: 'Exercise',
-                                    ),
-                                  ),)
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Row(
-                                children: [
-                                  Expanded(flex: 1, child: TextField(
-                                    onChanged: (text) {
-                                      if (text.isNotEmpty) {
-                                        exercises[i] = Exercise(muscleGroup: exercises[i].muscleGroup, name: exercises[i].name, repCount: exercises[i].repCount, setCount: int.parse(text));
-                                      }
-                                    },
-                                    keyboardType: TextInputType.number,
-                                    inputFormatters: [
-                                      FilteringTextInputFormatter.digitsOnly
-                                    ],
-                                    decoration: const InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      hintText: 'Set count',
-                                    ),
-                                  )),
-                                  const SizedBox(
-                                    width: 15,
-                                  ),
-                                  Expanded(flex: 1, child: TextField(
-                                    onChanged: (text) {
-                                      if (text.isNotEmpty) {
-                                        exercises[i] = Exercise(muscleGroup: exercises[i].muscleGroup, name: exercises[i].name, repCount: int.parse(text), setCount: exercises[i].setCount);
-                                      }
-                                    },
-                                    keyboardType: TextInputType.number,
-                                    inputFormatters: <TextInputFormatter>[
-                                      FilteringTextInputFormatter.digitsOnly
-                                    ],
-                                    decoration: const InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      hintText: 'Rep count',
-                                    ),
-                                  ))
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                            ],
-                          )
+                                ListView(
+                                  shrinkWrap: true,
+                                  children: sportsDay.exercises.map((Exercise exercise) =>
+                                      Column(
+                                        children: [
+                                          CircleAvatar(child: Text((sportsDay.exercises.indexOf(exercise) + 1).toString())),
+                                          const SizedBox(
+                                            height: 15,
+                                          ),
+                                          Row(
+                                            children: [
+                                              Expanded(flex: 0, child: DropdownButton<String>(
+                                                value: exercise.muscleGroup,
+                                                hint: const Text('Muscle group'),
+                                                items: <String>['Shoulders', 'Biceps', 'Triceps', 'Chest', 'Abs', 'Back', 'Legs'].map((String value) {
+                                                  return DropdownMenuItem<String>(
+                                                    value: value,
+                                                    child: Text(value),
+                                                  );
+                                                }).toList(),
+                                                onChanged: (String? newValue) {
+                                                  setState(() {
+                                                    exercise = Exercise(
+                                                        muscleGroup: newValue ?? '',
+                                                        name: exercise.name,
+                                                        repCount: exercise.repCount,
+                                                        setCount: exercise.setCount);
 
-                      ],
-                    )
-                ),
-                Padding(padding: const EdgeInsets.only(top: 20.0), child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Theme.of(context).primaryColor,
-                      minimumSize: Size(w / 1.1, h / 15)),
-                  onPressed: () => {
-                    setState(() => {
-                      exercises.add(Exercise(muscleGroup: 'Shoulders', name: '', repCount: 0, setCount: 0))
-                    }),
-                    animateToIndex(exercises.length)
-                  },
-                  child: const Text("Add exercise"),
-                ),),
-                Padding(padding: const EdgeInsets.only(top: 20.0), child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Theme.of(context).primaryColor,
-                      minimumSize: Size(w / 1.1, h / 15)),
-                  onPressed: () => {
-                    setState(() => {
-                      exercises.add(Exercise(muscleGroup: 'Shoulders', name: '', repCount: 0, setCount: 0))
-                    }),
-                    animateToIndex(exercises.length)
-                  },
-                  child: const Text("Save"),
-                ),)
+                                                  });
+                                                },
+                                              ),),
+                                              const SizedBox(
+                                                width: 15,
+                                              ),
+                                              Expanded(flex: 6, child: TextField(
+                                                onChanged: (text) {
+                                                  exercise = Exercise(
+                                                      muscleGroup: exercise.muscleGroup,
+                                                      name: text,
+                                                      repCount: exercise.repCount,
+                                                      setCount: exercise.setCount);
+                                                },
+                                                decoration: const InputDecoration(
+                                                  border: OutlineInputBorder(),
+                                                  hintText: 'Exercise',
+                                                ),
+                                              ),)
+                                            ],
+                                          ),
+                                          const SizedBox(
+                                            height: 15,
+                                          ),
+                                          Row(
+                                            children: [
+                                              Expanded(flex: 1, child: TextField(
+                                                onChanged: (text) {
+                                                  if (text.isNotEmpty) {
+                                                    exercise = Exercise(
+                                                        muscleGroup: exercise.muscleGroup,
+                                                        name: exercise.name,
+                                                        repCount: exercise.repCount,
+                                                        setCount: int.parse(text));
+                                                  }
+                                                },
+                                                keyboardType: TextInputType.number,
+                                                inputFormatters: [
+                                                  FilteringTextInputFormatter.digitsOnly
+                                                ],
+                                                decoration: const InputDecoration(
+                                                  border: OutlineInputBorder(),
+                                                  hintText: 'Set count',
+                                                ),
+                                              )),
+                                              const SizedBox(
+                                                width: 15,
+                                              ),
+                                              Expanded(flex: 1, child: TextField(
+                                                onChanged: (text) {
+                                                  if (text.isNotEmpty) {
+                                                    exercise = Exercise(
+                                                        muscleGroup: exercise.muscleGroup,
+                                                        name: exercise.name,
+                                                        repCount: int.parse(text),
+                                                        setCount: exercise.setCount);
+                                                  }
+                                                },
+                                                keyboardType: TextInputType.number,
+                                                inputFormatters: <TextInputFormatter>[
+                                                  FilteringTextInputFormatter.digitsOnly
+                                                ],
+                                                decoration: const InputDecoration(
+                                                  border: OutlineInputBorder(),
+                                                  hintText: 'Rep count',
+                                                ),
+                                              ))
+                                            ],
+                                          ),
+                                          const SizedBox(
+                                            height: 15,
+                                          ),
+                                        ],
+                                      )
+                                  ).toList(),
+                                  // children: [
+                                  //
+                                  //   for( int i = 0 ; i < sportsDay.exercises.length ; i++)
+                                  //
+                                  // ],
+                                )
+                              ],
+                            )
+                        ).toList()
+                    )),
+                Row(children: [
+                  Expanded(child: Padding(padding: const EdgeInsets.only(top: 20.0), child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
+                        minimumSize: Size(w / 1.1, h / 15)),
+                    onPressed: () => {
+                      setState(() => {
+                        sportsDays[sportsDays.length - 1].exercises.add(Exercise(muscleGroup: 'Shoulders', name: '', repCount: 0, setCount: 0)),
+                        scrollDown()
+                      }),
+
+                    },
+                    child: const Text("Add exercise"),
+                  ),),),
+                  const SizedBox(width: 15,),
+                  Expanded(child: Padding(padding: const EdgeInsets.only(top: 20.0), child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
+                        minimumSize: Size(w / 1.1, h / 15)),
+                    onPressed: () => {
+                      setState(() => {
+                        sportsDays.add(SportsDay(exercises: List.filled(1, Exercise(muscleGroup: 'Shoulders', name: '', repCount: 0, setCount: 0), growable: true))),
+                        scrollDown()
+                      }),
+
+                    },
+                    child: const Text("Add day"),
+                  ),),),
+                  const SizedBox(width: 15,),
+                  Expanded(child: Padding(padding: const EdgeInsets.only(top: 20.0), child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
+                        minimumSize: Size(w / 1.1, h / 15)),
+                    onPressed: () => {
+                    },
+                    child: const Text("Save"),
+                  ),))
+                ],)
               ],
             )
         )
