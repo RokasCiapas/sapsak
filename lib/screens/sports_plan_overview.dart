da63c2290d6cc4965bf0c477598ef887d2a2f7fc
Rokas Ciapas <rokas.ciapas@nortal.com>
2022-09-15 23:18:25 +0300

    table improvements

--- lib/screens/sports_plan_overview.dart
+++ lib/screens/sports_plan_overview.dart
@@ -23,73 +23,70 @@ class SportsPlanOverview extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: sportsPlan.sportsDays.map((SportsDay day) {
           Map<int, Multiset> multisets = day.multisets;
-          List<DataRow> rows = [];
+          List<TableRow> rows = [
+            const TableRow(
+                children: [
+                  Padding(padding: EdgeInsets.all(5), child: Text('Muscle group'),),
+                  Padding(padding: EdgeInsets.all(5), child: Text('Exercise'),),
+                  Padding(padding: EdgeInsets.all(5), child: Text('Sets'),),
+                  Padding(padding: EdgeInsets.all(5), child: Text('Weight'),),
+                ]
+            ),
+          ];
 
           for (var m in multisets.values) {
             List<Exercise> multiset = m.multiset;
 
-            rows.add(DataRow(
-                cells: [
-                  DataCell(
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: multiset.map((e) {
-                          return Padding(padding: const EdgeInsets.all(5), child: Text(e.muscleGroup));
-                        }).toList(),
-                      )
+            rows.add(TableRow(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: multiset.map((e) {
+                      return Padding(padding: const EdgeInsets.all(5), child: Text(e.muscleGroup));
+                    }).toList(),
                   ),
-                  DataCell(
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: multiset.map((e) {
-                          return Padding(padding: const EdgeInsets.all(5), child: Text(e.name));
-                        }).toList(),
-                      )
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: multiset.map((e) {
+                      return Padding(padding: const EdgeInsets.all(5), child: Text(e.name));
+                    }).toList(),
                   ),
-                  DataCell(
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: multiset.map((e) {
-                          return Padding(padding: const EdgeInsets.all(5), child: Text(e.setCount.toString()));
-                        }).toList(),
-                      )
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: multiset.map((e) {
+                      return Padding(padding: const EdgeInsets.all(5), child: Text('${e.setCount}x${e.repCount}'));
+                    }).toList(),
                   ),
-                  DataCell(
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: multiset.map((e) {
-                          return Padding(padding: const EdgeInsets.all(5), child: Text(e.repCount.toString()));
-                        }).toList(),
-                      )
-                  ),
-                  DataCell(
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: multiset.map((e) {
-                          return Padding(padding: const EdgeInsets.all(5), child: Text(e.weight));
-                        }).toList(),
-                      )
-                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: multiset.map((e) {
+                      return Padding(padding: const EdgeInsets.all(5), child: Text(e.weight));
+                    }).toList(),
+                  )
                 ])
             );
           }
 
-            return DataTable(
-              showCheckboxColumn: false,
-              dataRowHeight: 90,
-              columns: const [
-                DataColumn(label: Text('Muscle group')),
-                DataColumn(label: Text('Exercise')),
-                DataColumn(label: Text('Set count')),
-                DataColumn(label: Text('Rep count')),
-                DataColumn(label: Text('Weight')),
-              ],
-              rows: rows
+          return Table(
+            columnWidths: const {
+              0: FixedColumnWidth(74),
+              1: FlexColumnWidth(3),
+              2: FixedColumnWidth(40),
+              3: FixedColumnWidth(60),
+            },
+            border: TableBorder(
+              top: BorderSide(color: Theme.of(context).colorScheme.secondary),
+              bottom: BorderSide(color: Theme.of(context).colorScheme.secondary),
+              right: BorderSide(color: Theme.of(context).colorScheme.secondary),
+              left: BorderSide(color: Theme.of(context).colorScheme.secondary),
+              horizontalInside: BorderSide(color: Theme.of(context).colorScheme.secondary),
+              verticalInside: BorderSide(color: Theme.of(context).colorScheme.secondary),
+            ),
+            children: rows,
           );
         }).toList(),
       ),
